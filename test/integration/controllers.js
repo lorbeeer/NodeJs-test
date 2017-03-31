eb6c6d4c2bbacf565ac6697dbffb37dd9ff2b2aa
lorbeeer <zinovievadrive@gmail.com>
2017-03-31 14:16:36 +0300

    Update controllers.js

--- test/integration/controllers.js
+++ test/integration/controllers.js
@@ -4,17 +4,16 @@ let assert = require('chai').assert
 let request = require('supertest-as-promised')
 
 let app = require('../../app')
-
-let email = 'integration_tests_' + Math.floor(Date.now() / 1000) + '@wartech.ua'
+let email = 'integration_test_' + Math.floor(Date.now() / 1000) + '@wartech.ua'
 let password = 'test'
 let name = 'My name'
- 
 
 describe('Authentication Controller', () => {
 
   it('should register a new user and return token', () => {
     let _token = null;
-    return request(app.listen()) 
+
+    return request(app)
       .post('/api/register')
       .send({ email, password, name })
       .expect(201)
@@ -37,33 +36,30 @@ describe('Authentication Controller', () => {
   });
 
   it('should return an error bad request if email is used', () => {
-    return request(app.listen())
+    return request(app)
       .post('/api/register')
       .send({ email, password, name})
       .expect(400);
   });
 
   it('should return an error bad request if email isn\'t specified', () => {
-    return request(app.listen())
+    return request(app)
       .post('/api/register')
       .send({ password, name })
       .expect(400);
   });
 
   it('should return an error bad request if password isn\'t specified', () => {
-    return request(app.listen())
+    return request(app)
       .post('/api/register')
       .send({ email, name })
       .expect(400);
   });
-
 });
 
 describe('Profile controller', () => {
 
   let _token = null;
-
-  
 
   before(() => {
     return request(app)
@@ -74,10 +70,7 @@ describe('Profile controller', () => {
         assert.ok(_token);
       });
   });
-  
 
-  
- 
   it('should fetch the profile info of existing user', () => {
     let _user = email;
     return request(app)
@@ -87,14 +80,11 @@ describe('Profile controller', () => {
       .then((data) => {
         assert.equal(data.body.email, _user);
       });
-    
   });
- 
+
   it('should return an error when token is not specified', () => {
     return request(app)
       .get('/api/profile')
-      .expect(401)
+      .expect(401);
   });
-  
 });
-
